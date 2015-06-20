969798bd45d30296b9c9a07cc47637eb92d29854
Feng Hao <howiefh@gmail.com>
2015-06-20 11:49:07 +0800

    support code.angularjs.org

--- bootstrap.js
+++ bootstrap.js
@@ -2,10 +2,14 @@ var Cc = Components.classes;
 var Ci = Components.interfaces;
 
 var host_changes = {
+	"code.angularjs.org":"libs.useso.com",
 	"ajax.googleapis.com":"ajax.lug.ustc.edu.cn",
 	"fonts.googleapis.com":"fonts.lug.ustc.edu.cn",
 	"themes.googleusercontent.com":"google-themes.lug.ustc.edu.cn"
 };
+var path_changes = {
+	"code.angularjs.org":"/js/angular.js"
+}
 
 var http_obs = {
 	observe: function(subject,topic,data){
@@ -16,6 +20,9 @@ var http_obs = {
 		{
 			var new_uri = uri.clone();
 			new_uri.host = host_changes[oldhost];
+            if (oldhost in path_changes) {
+                new_uri.path = path_changes[oldhost] + new_uri.path;
+            }
 			//ref: http://stackoverflow.com/questions/24947233/use-http-on-modify-request-to-redirect-url-firefox-add-on
 			httpChannel.redirectTo(new_uri);
 			console.log("uri change from " + uri.spec + " to " + new_uri.spec);
